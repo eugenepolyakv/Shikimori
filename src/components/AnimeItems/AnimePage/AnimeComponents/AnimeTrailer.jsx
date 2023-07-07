61ddd776bb93ec660cc597003275a67391fc0428
Eugene Polyakov <jenick12345@yandex.ru>
2023-07-07 14:06:05 +0300

    video player styled + overlay for a video player has been added. Template for an anime page partially done

--- src/components/AnimeItems/AnimePage/AnimeComponents/AnimeTrailer.jsx
+++ src/components/AnimeItems/AnimePage/AnimeComponents/AnimeTrailer.jsx
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from '../AnimePage.module.css';
+import closingCross from '../../../../assets/closingCross.svg';
+import playButton from '../../../../assets/playButton.png';
 const AnimeTrailer = () => {
+    const [openModal, setOpenModal] = useState(false);
     return (
         <div>
-            <div className={styles.title}>TRAILER</div>
-            <iframe
-                width="80%"
-                height="600px"
-                src="https://www.youtube.com/embed/qig4KOK2R2g?enablejsapi=1&wmode=opaque&autoplay=1"
-                style={{ margin: '0 10%' }}
-            ></iframe>
+            <div style={{ display: 'inline-block' }}>
+                <div className={styles.title}>TRAILER</div>
+                <div
+                    onClick={() => setOpenModal(true)}
+                    className={styles.smallVideoContainer}
+                    style={{
+                        backgroundImage: `url(https://img.youtube.com/vi/bJVyIXeUznY/sddefault.jpg)`,
+                    }}
+                >
+                    <div className={styles.overlay1}>
+                        <img src={playButton} />
+                    </div>
+                </div>
+                {openModal ? (
+                    <div
+                        onClick={() => setOpenModal(false)}
+                        className={styles.overlay}
+                    >
+                        <div
+                            onClick={(e) => e.stopPropagation()}
+                            className={styles.modalContainer}
+                        >
+                            <div
+                                className={styles.videoCross}
+                                onClick={() => setOpenModal(false)}
+                            >
+                                {/* <img
+                                className={styles.videoCross}
+                                src={closingCross}
+                            /> */}
+                                ×
+                            </div>
+                            <iframe
+                                className={styles.video}
+                                width="80%"
+                                height="600px"
+                                src="https://www.youtube.com/embed/bJVyIXeUznY?enablejsapi=1&wmode=opaque&autoplay=1"
+                            ></iframe>
+                        </div>
+                    </div>
+                ) : null}
+            </div>
         </div>
     );
 };
