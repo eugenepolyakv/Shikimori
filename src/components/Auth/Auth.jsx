04724d79fa49d29c946d21ec6a3c0a8c0e82d808
Eugene Polyakov <jenick12345@yandex.ru>
2023-07-19 14:57:40 +0300

    auth and register form layout done

--- src/components/Auth/Auth.jsx
+++ src/components/Auth/Auth.jsx
@@ -1,18 +1,69 @@
 import React, { useState } from 'react';
 import { useGetUserTokenMutation } from '../../redux/api/authApi';
-
+import styles from './Auth.module.scss';
+import lock from '../../assets/lock.svg';
 const Auth = () => {
-    // const [isButtonClicked, setButtonClicked] = useState(false);
-    // const { data } = useGetUserTokenQuery(skipToken);
-    // const { data } = useGetUserTokenMutation();
-    // console.log(data);
-    const [getToken, meta] = useGetUserTokenMutation();
-    console.log(meta);
+    const [isLoginShown, setLoginForm] = useState(true);
+    // const [getToken, meta] = useGetUserTokenMutation();
+    // console.log(meta);
     return (
-        <div>
-            <button onClick={() => getToken({ value: 1 })}>
-                GET USER TOKEN
-            </button>
+        <div className={styles.mainDiv}>
+            <div className={styles.formStructor}>
+                <div
+                    className={
+                        isLoginShown
+                            ? styles.login
+                            : `${styles.login} ${styles.slideUp}`
+                    }
+                    onClick={() => setLoginForm(true)}
+                >
+                    <div className={styles.center}>
+                        <h2 className={styles.formTitle} id="login">
+                            {isLoginShown ? null : <span>or</span>}Sign in
+                        </h2>
+                        <div className={styles.formHolder}>
+                            <input
+                                type="login"
+                                className={styles.input}
+                                placeholder="Login"
+                            />
+                            <input
+                                type="password"
+                                className={styles.input}
+                                placeholder="Password"
+                            />
+                        </div>
+                        <button className={styles.submitBtn}>Sign in</button>
+                    </div>
+                </div>
+                <div
+                    className={
+                        isLoginShown
+                            ? `${styles.signup} ${styles.slideUp}`
+                            : styles.signup
+                    }
+                    onClick={() => setLoginForm(false)}
+                >
+                    <div className={styles.center}>
+                        <h2 className={styles.formTitle} id="signup">
+                            {isLoginShown ? <span>or</span> : null}Sign up
+                        </h2>
+                        <div className={styles.formHolder}>
+                            <input
+                                type="login"
+                                className={styles.input}
+                                placeholder="Login"
+                            />
+                            <input
+                                type="password"
+                                className={styles.input}
+                                placeholder="Password"
+                            />
+                        </div>
+                        <button className={styles.submitBtn}>Sign up</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
